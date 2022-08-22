0da9052d93f46501191bdcec4c7b282a4d9a2f28
Omar <omardanielesquivel@gmail.com>
2022-08-22 04:48:30 -0500

    Scroll App

---
+++ js/app_scroll.js
@@ -0,0 +1,25 @@
+(() => {
+
+
+    'use strict';
+
+
+    let mainUbication = window.scrollY;
+    const desktopMenu = document.querySelector('.desktop-menu__navbar');
+    const mobileMenu = document.querySelector('.mobile-menu__navbar ');
+
+
+    window.onscroll = function() {
+        let displacement = window.scrollY;
+        if (mainUbication >= displacement) {
+            desktopMenu.style.top = '0';
+            mobileMenu.style.top = '0';
+        } else {
+            desktopMenu.style.top = '-20rem';
+            mobileMenu.style.top = '-20rem';
+        }
+        mainUbication = displacement;        
+    }
+
+
+})();
