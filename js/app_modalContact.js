2474cca6c243615e43b19958891f58988a120b1e
Omar <omardanielesquivel@gmail.com>
2022-09-02 04:58:53 -0500

    social cards

---
+++ js/app_modalContact.js
@@ -0,0 +1,8 @@
+const socialItems = document.querySelectorAll('.social-section__item'),
+      socialButtons = document.querySelectorAll('.modal-button');
+
+socialItems.forEach(function(element, index) {
+    element.addEventListener('click', () => {
+        socialButtons[index].click();
+    });
+});
